c09f612e338acdc8e96222ac89b8e7aa0f5576fc
adammathewvaughn <adammathewvaughn@gmail.com>
2021-11-21 21:18:10 -0600

    css specificity conflicts need addressing

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,11 @@ function App() {
           </a>
           </p>
           </body>
-          <div id="Footer"></div>
+<div id="Footer"><p>Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
+</p></div>;
           </div>
           )
 }
+
 
 export default App
