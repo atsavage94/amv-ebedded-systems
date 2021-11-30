24b63bb0da235e60a0d588b2b0ab76d3771b6da2
adammathewvaughn <adammathewvaughn@gmail.com>
2021-11-29 23:26:36 -0600

    addressed analytics issue

---
+++ src/client/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+import icon from "./favicon.png";
+
+const Footer = () => {
+return (
+<div id="Footer">
+
+<p>
+    Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
+</p>
+
+</div>
+
+
+)
+
+
+
+}
+
+export default Footer;
