52b136ed32fe55e6b8247098d49fb7004ef7be80
adammathewvaughn <adammathewvaughn@gmail.com>
2021-11-30 00:19:46 -0600

    adjusted analytics tags

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import logo from './logo.png'
 import icon from './favicon.svg'
 import Footer from './Footer'
 import './App.css'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-MW7CTZK'
+}
+TagManager.initialize(tagManagerArgs)
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,19 +16,19 @@ function App() {
   return (
     <div className="App">
       <body className="App-body">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h4>"Automating a better tomorrow"</h4>
-      <p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/J4lrDOR73cM" title="YouTube video player"
-      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen></iframe>
-             <a
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4>"Automating a better tomorrow"</h4>
+        <p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/J4lrDOR73cM" title="YouTube video player"
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+          <a
             className="App-link"
             href="https://blog.adafruit.com/2020/10/16/building-embedded-linux-systems-embedded-linux-ee-jaydcarlson/"
             target="_blank"
             rel="noopener noreferrer"
           >
-           About Embedded Systems
+            About Embedded Systems
           </a>
           {' | '}
           <a
@@ -31,14 +37,15 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Contact Us 
+            Contact Us
           </a>
-          </p>
-          </body>
-<div id="Footer"><p>Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
-</p></div>
-          </div>
-          )
+        </p>
+      </body>
+      <div id="Footer"><p>Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
+      </p>
+      </div>
+    </div>
+  )
 }
 
 
