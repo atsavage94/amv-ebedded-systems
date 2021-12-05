490aef4ed46ccef68a4fa6ec52446ba00752910f
Anthony Savage <46229557+atsavage94@users.noreply.github.com>
2021-12-04 23:46:47 -0600

    Updated Footer.jsx with footer tag

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -5,19 +5,14 @@ import icon from "./favicon.png";
 
 const Footer = () => {
 return (
-<div id="Footer">
-
-<p>
-    Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
-</p>
-
-</div>
-
-
+<footer id="footer">
+    <div>
+    <p>
+     Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
+    </p>
+    </div>
+</footer>
 )
-
-
-
 }
 
 export default Footer;
