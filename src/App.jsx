f4680eb0d2fcb7745fcbcb2bf1eceff54927b060
adammathewvaughn <adammathewvaughn@gmail.com>
2021-12-05 15:54:35 -0600

    committing to merge locally, I think

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import logo from './logo.png'
 import icon from './favicon.svg'
-import Footer from './Footer'
 import './App.css'
 import TagManager from 'react-gtm-module'
 
