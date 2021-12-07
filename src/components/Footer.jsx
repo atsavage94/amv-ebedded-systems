e5dbede6540e5deaf4f20b32b57abadaf2d2f619
Anthony Savage <46229557+atsavage94@users.noreply.github.com>
2021-12-06 22:07:53 -0600

    Updated file to test layout with new footer and an additional .js file to components folder (also new)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import icon from "./favicon.png";
 
 const Footer = () => {
 return (
-<footer id="footer">
-    <div>
+<div className="footer">
+    <footer>
     <p>
      Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
     </p>
-    </div>
-</footer>
+    </footer>
+</div>
 )
-}
+};
 
 export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import {
+  App,
+  Footer,
+} from './components'; 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 )
