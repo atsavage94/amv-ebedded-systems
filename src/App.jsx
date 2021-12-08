846889dcdf2ecff840ef78d31703cf21d3d5b20e
adammathewvaughn <adammathewvaughn@gmail.com>
2021-12-08 03:03:46 -0600

    added a bio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import logo from './logo.png'
-<<<<<<< HEAD
-import icon from './favicon.svg'
-=======
->>>>>>> 3e913b1b5917a5864136899a31cc05154dc79a7e
 import './App.css'
 import TagManager from 'react-gtm-module'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const i = <FontAwesomeIcon icon={faCoffee} />
 
 const tagManagerArgs = {
   gtmId: 'GTM-MW7CTZK'
@@ -14,37 +14,37 @@ TagManager.initialize(tagManagerArgs)
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="header">
         <div className=""></div>
         <div className=""></div>
-        <img src={logo} className="App-logo" alt="logo" />  
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
       <body className="App-body">
-<div className="amv-logo">
-    <p className="name">Adam Mathew Vaughn</p>
-    <hr />
-    <p className="brand">Embedded<br />Systems</p>
-</div>
-    <h4>"Automating a better tomorrow"</h4>
-        <p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/J4lrDOR73cM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          <a
+        <div className="amv-logo">
+          <p className="name">Adam Mathew Vaughn</p>
+          <hr />
+          <p className="brand">Embedded<br />Systems</p>
+        </div>
+        <h4>"Automating a better tomorrow"</h4>
+        <p> I'm Adam and I'm a full-stack web developer and embedded systems engineer. I'm currently working to combine my recent full-stack web development education with my background in construction and my love for hardware engineering. 
+            Embedded systems is the natural arris of the adjacent technological disciplines, and I'm excited to begin my path in the field. The juxtaposition of hardware and software has never been more at the forefront of our lives with the prevalence of the IoT and micro-controlled embedded systems via Raspberry Pi and Arduino.
+             I feel like I have a chance to contribute to a better world through embedded systems and home automation, and I'm eagerly working towards the opportunity to do so.</p>
+          <div className="LinksDiv"><a
             className="App-link"
-            href="https://blog.adafruit.com/2020/10/16/building-embedded-linux-systems-embedded-linux-ee-jaydcarlson/"
+            href="https://www.linkedin.com/in/vaughn-embedded-systems"
             target="_blank"
             rel="noopener noreferrer"
           >
-            About Embedded Systems
+            Let's Connect<i className="fas fa-coffee"> {i}</i>
           </a>
           <a
             className="App-link"
             href="mailto:adam@adammathewvaughn.com"
             target="_blank"
             rel="noopener noreferrer"
-          > Email  
+          > Email
           </a>
           <a
             className="App-link"
@@ -59,8 +59,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >Call
-          </a>
-        </p>
+          </a></div>
       </body>
     </div>
   )
