be716a639f78b7864574b3b99f7553abe9d5fe7e
Anthony Savage <46229557+atsavage94@users.noreply.github.com>
2021-12-07 18:43:09 -0600

    updated footer id

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import icon from "./favicon.png";
 
 const Footer = () => {
 return (
-<div id="footers">
+<div id="footers-container">
     <footer className="Footer">
     <p>
      Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
