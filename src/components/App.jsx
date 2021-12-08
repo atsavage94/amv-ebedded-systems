1eb3defc4ad4911e4794dc727495fade8393605f
Anthony Savage <46229557+atsavage94@users.noreply.github.com>
2021-12-07 18:35:34 -0600

    updated main, app, and footer JSX files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -57,7 +57,7 @@ function App() {
       </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import icon from "./favicon.png";
 
 const Footer = () => {
 return (
-<div className="footer">
-    <footer>
+<div id="footers">
+    <footer className="Footer">
     <p>
      Run <code> npm init vite@latest</code> in your command line to try ViteJS<img src={icon} className="App-ico" alt="ico" height="20px" width="20px" />
     </p>
     </footer>
 </div>
-)
-};
+);
+}
 
 export default Footer;
